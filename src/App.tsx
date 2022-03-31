dd80b45d5c5ccbb865eaeb3b84bec6e5c88504f1
antonorlov <fogelo@yandex.ru>
2022-03-31 19:48:20 +0300

    todolists

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,160 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
+import {v1} from 'uuid';
 
 
 export function App() {
+
+    const [todolists, setTodolists] = useState([
+        {
+            id: v1(), title: 'what to learn', tasks: [
+                {id: v1(), title: 'html', isDone: true},
+                {id: v1(), title: 'css', isDone: true},
+                {id: v1(), title: 'react', isDone: true},
+                {id: v1(), title: 'redux', isDone: false},]
+        },
+        {
+            id: v1(), title: 'what to buy', tasks: [
+                {id: v1(), title: 'milk', isDone: true},
+                {id: v1(), title: 'meat', isDone: true},
+                {id: v1(), title: 'egs', isDone: false},]
+        },
+    ])
+
+    const addTodolist = (title: string) => {
+        const newTodolist = {
+            id: v1(),
+            title: title,
+            tasks: []
+        }
+        setTodolists([...todolists, newTodolist])
+    }
+    const changeTodolistTitle = (todolistId: string, title: string) => {
+        const todolist = todolists.find(tl => tl.id === todolistId)
+        if (todolist) {
+            todolist.title = title
+        }
+        setTodolists([...todolists])
+    }
+
+    const removeTodolist = (todolistId: string) => {
+        setTodolists(todolists.filter(tl => tl.id !== todolistId))
+    }
+
+    const addTask = (todolistId: string, title: string) => {
+        const newTask = {
+            id: v1(),
+            title: title,
+            isDone: false
+        }
+        const todolist = todolists.find(tl => tl.id === todolistId)
+        if (todolist) {
+            todolist.tasks = [...todolist.tasks, newTask]
+            setTodolists([...todolists])
+        }
+    }
+
+    const changeTaskTitle = (todolistId: string, taskId: string, title: string) => {
+        const todolist = todolists.find(tl => tl.id === todolistId)
+        if (todolist) {
+            const task = todolist.tasks.find(t => t.id === taskId)
+            if (task) {
+                task.title = title
+            }
+        }
+        setTodolists([...todolists])
+    }
+
+    const changeTaskStatus = (todolistId: string, taskId: string, isDone: boolean) => {
+        const todolist = todolists.find(tl => tl.id === todolistId)
+        if (todolist) {
+            const task = todolist.tasks.find(t => t.id === taskId)
+            if (task) {
+                task.isDone = isDone
+            }
+        }
+        setTodolists([...todolists])
+    }
+    const removeTask = (todolistId: string, taskId: string) => {
+        const todolist = todolists.find(tl => tl.id === todolistId)
+        if (todolist) {
+            todolist.tasks = todolist.tasks.filter(t => t.id !== taskId)
+        }
+        setTodolists([...todolists])
+    }
+
     return (
         <div className="App">
-            <EditableSpan title={'hard'} changeTitle={() => {
-            }}/>
-
+            <AddItemForm addItem={addTodolist}/>
+            {todolists.map(tl => <Todolist key={tl.id}
+                                           todolistTitle={tl.title}
+                                           todolistId={tl.id}
+                                           changeTodolistTitle={changeTodolistTitle}
+                                           removeTodolist={removeTodolist}
+                                           tasks={tl.tasks}
+                                           addTask={addTask}
+                                           changeTaskStatus={changeTaskStatus}
+                                           removeTask={removeTask}/>)}
         </div>
     );
+}
+
+
+type TaskType = {
+    id: string
+    title: string
+    isDone: boolean
+}
+
+type TodolistPT = {
+    todolistTitle: string
+    todolistId: string
+    changeTodolistTitle: (todolistId: string, title: string) => void
+    removeTodolist: (todolistId: string) => void
+    tasks: TaskType[]
+    addTask: (todolistId: string, title: string) => void
+    changeTaskStatus: (todolistId: string, taskId: string, isDone: boolean) => void
+    removeTask: (todolistId: string, taskId: string) => void
+}
+
+const Todolist = (props: TodolistPT) => {
+    const changeTodolistTitle = (title: string) => {
+        props.changeTodolistTitle(props.todolistId, title)
+    }
+    const addTask = (title: string) => {
+        props.addTask(props.todolistId, title)
+    }
+
+    const removeTodolist = () => {
+        props.removeTodolist(props.todolistId)
+    }
+
+    return (
+        <div>
+            <h3>
+                <EditableSpan title={props.todolistTitle} changeTitle={changeTodolistTitle}/>
+                <button onClick={removeTodolist}>x</button>
+            </h3>
+            <AddItemForm addItem={addTask}/>
+            {props.tasks.map(t => {
+                const changeTaskStatus = (e: ChangeEvent<HTMLInputElement>) => {
+                    props.changeTaskStatus(props.todolistId, t.id, e.currentTarget.checked)
+                }
+                const removeTask = () => {
+                    props.removeTask(props.todolistId, t.id)
+                }
+                return (
+                    <div key={t.id}>
+                        <button onClick={removeTask}>x</button>
+                        <input checked={t.isDone}
+                               onChange={changeTaskStatus}
+                               type="checkbox"/>
+                        {t.title}
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
 
 
@@ -26,16 +171,61 @@ const EditableSpan = (props: EditableSpanPT) => {
     }
     const deActivateEditMode = () => {
         setEditMode(false)
+        props.changeTitle(title)
     }
     return (
         <div>
             {editMode
-                ? <input type="text" value={title}
+                ? <input type="text"
+                         value={title}
                          autoFocus
                          onBlur={deActivateEditMode}
                          onChange={(e) => setTitle(e.currentTarget.value)}/>
                 : <span onDoubleClick={activateEditMode}>{title}</span>
             }
+        </div>
+    )
+}
+
+type AddItemFormPT = {
+    addItem: (title: string) => void
+}
+
+const AddItemForm = (props: AddItemFormPT) => {
+    const [title, setTitle] = useState('')
+    const [error, setError] = useState(false)
+
+    const addItem = () => {
+        if (title) {
+            props.addItem(title)
+            setTitle('')
+        } else {
+            setError(true)
+        }
+    }
+
+    const changeItem = (e: ChangeEvent<HTMLInputElement>) => {
+        if (error) {
+            setError(false)
+        }
+        setTitle(e.currentTarget.value)
+    }
+
+    const errorInputStyle = {
+        border: '2px solid red'
+    }
+    const errorSpanStyle = {
+        color: 'red'
+    }
+    return (
+        <div>
+            <input type="text"
+                   value={title}
+                   autoFocus
+                   style={error ? errorInputStyle : {}}
+                   onChange={changeItem}/>
+            {error ? <div style={errorSpanStyle}>field is required</div> : ''}
+            <button onClick={addItem}>+</button>
         </div>
     )
 }
