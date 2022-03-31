51cd755687451ca221e697e1d229e58264d2a26f
antonorlov <fogelo@yandex.ru>
2022-03-31 10:24:23 +0300

    OnOff

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import React, {useState} from 'react';
 import './App.css';
 import {UncontrolledRating} from './Rating/UncontrolledRating';
 import {ControlledRating} from './Rating/ControlledRating';
+import {UncontrolledOnOff} from './OnOff/UncontrolledOnOff';
+import {ControlledOnOff} from './OnOff/ControlledOnOff';
 
 
 export function App() {
     const [ratingValue, setRatingValue] = useState(0)
+    const [on, setOn] = useState(false)
     return (
         <div className="App">
             <UncontrolledRating/>
             <ControlledRating value={ratingValue} setRatingValue={setRatingValue}/>
+
+            <UncontrolledOnOff/>
+            <ControlledOnOff on={on} setOn={setOn}/>
         </div>
     );
 }
---
+++ src/OnOff/ControlledOnOff.tsx
@@ -0,0 +1,28 @@
+type ControlledOnOffPT = {
+    on: boolean
+    setOn: (on: boolean) => void
+}
+export const ControlledOnOff = (props: ControlledOnOffPT) => {
+
+    const styleOn = {
+        backgroundColor: 'green'
+    }
+    const styleOff = {
+        backgroundColor: 'red'
+    }
+
+    const styleIndicate = {
+        width: '20px',
+        height: '20px',
+        borderRadius: '50%',
+        backgroundColor: props.on ? 'green' : 'red',
+        display: 'inline-block'
+    }
+    return (
+        <div style={{display: 'flex'}}>
+            <button style={props.on ? styleOn : {}} onClick={() => props.setOn(true)}>on</button>
+            <button style={!props.on ? styleOff : {}} onClick={() => props.setOn(false)}>off</button>
+            <div style={styleIndicate}></div>
+        </div>
+    )
+}
