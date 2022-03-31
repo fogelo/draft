d5da893c6ba803110343afd8f3020d6db6109ca6
antonorlov <fogelo@yandex.ru>
2022-03-31 14:55:29 +0300

    useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,11 @@
-import React, {useCallback, useReducer, useState} from 'react';
+import React from 'react';
 import './App.css';
-import {UncontrolledRating} from './Rating/UncontrolledRating';
-import {ControlledRating} from './Rating/ControlledRating';
-import {UncontrolledOnOff} from './OnOff/UncontrolledOnOff';
-import {ControlledOnOff} from './OnOff/ControlledOnOff';
 
-
-const reducerApp = (state: any, action: any) => {
-    switch (action.type) {
-        case 'SET-ON': {
-            return {...state, on: action.on}
-        }
-        case 'SET-RATING': {
-            return {...state, ratingValue: action.ratingValue}
-        }
-        default: {
-            return state
-        }
-    }
-}
 
 export function App() {
-    const [state, dispatch] = useReducer(reducerApp, {on: false, ratingValue: 0})
-
-    const setRatingValue = useCallback(dispatch,[])
-
     return (
         <div className="App">
-            <UncontrolledRating/>
-            <ControlledRating value={state.ratingValue}
-                              setRatingValue={setRatingValue}/>
 
-            <UncontrolledOnOff/>
-            <ControlledOnOff on={state.on} setOn={(on)=>dispatch({type: 'SET-ON', on: on})}/>
         </div>
     );
 }
