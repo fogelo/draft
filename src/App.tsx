772a98bfbdcbed0947788d1c27556db04c667e66
antonorlov <fogelo@yandex.ru>
2022-03-31 15:09:19 +0300

    clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 
 export function App() {
     return (
         <div className="App">
-
+            <Clock/>
         </div>
     );
 }
 
+const standartValue = (value: any) => {
+    return value < 10 ? '0' + value : value
+}
+
+const Clock = () => {
+    const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+        setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+    }, [])
+    return (
+        <div>
+            {standartValue(date.getHours())}:
+            {standartValue(date.getMinutes())}:
+            {standartValue(date.getSeconds())}
+        </div>
+    )
+}
