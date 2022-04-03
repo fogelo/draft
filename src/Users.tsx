221fc367c05d2393caaf09e1f09c1375453138e1
antonorlov <fogelo@yandex.ru>
2022-04-03 16:21:43 +0300

    follow, unfollow

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {setUsersAC} from './redux/users-reducer';
+import {followAC, setUsersAC, unfollowAC} from './redux/users-reducer';
 import axios from 'axios';
 
 class Users extends React.Component<any> {
@@ -20,6 +20,10 @@ class Users extends React.Component<any> {
                             <div>name: {u.name}</div>
                             <div>photo: <img src={u.photos.small} alt="нет фото"/></div>
                             <div>status: {u.status ? u.status : 'нет статуса'}</div>
+                            {u.followed
+                                ? <button onClick={() => this.props.unfollow(u.id)}>unfollow</button>
+                                : <button onClick={() => this.props.follow(u.id)}>follow</button>
+                            }
                         </div>
                     )
                 })}
@@ -39,6 +43,12 @@ const mapDispatchToProps = (dispatch: any) => {
     return {
         setUsers: (users: any) => {
             dispatch(setUsersAC(users))
+        },
+        follow: (userId: any) => {
+            dispatch(followAC(userId))
+        },
+        unfollow: (userId: any) => {
+            dispatch(unfollowAC(userId))
         }
     }
 }
--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -7,6 +7,12 @@ export const usersReducer = (state: any = initState, action: any) => {
         case 'SET-USERS': {
             return {...state, users: action.users}
         }
+        case 'FOLLOW': {
+            return {...state, users: state.users.map((u: any) => u.id === action.userId ? {...u, followed: true} : u)}
+        }
+        case 'UNFOLLOW': {
+            return {...state, users: state.users.map((u: any) => u.id === action.userId ? {...u, followed: false} : u)}
+        }
         default: {
             return state
         }
@@ -15,3 +21,5 @@ export const usersReducer = (state: any = initState, action: any) => {
 
 
 export const setUsersAC = (users: any) => ({type: 'SET-USERS', users})
+export const followAC = (userId: any) => ({type: 'FOLLOW', userId})
+export const unfollowAC = (userId: any) => ({type: 'UNFOLLOW', userId})
