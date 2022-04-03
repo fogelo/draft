871568ba768a761e495a6991da56d93ad6faeb57
antonorlov <fogelo@yandex.ru>
2022-04-03 18:27:02 +0300

    pagination

--- src/Users.tsx
+++ src/Users.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {followAC, setUsersAC, unfollowAC} from './redux/users-reducer';
+import {followAC, setTotalUsersCountAC, setUsersAC, toggleCurrentPageAC, unfollowAC} from './redux/users-reducer';
 import axios from 'axios';
 
 class Users extends React.Component<any> {
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users/')
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=1&count=${this.props.usersCount}`)
             .then(response => {
                 this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged(currentPage: any) {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${this.props.usersCount}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.toggleCurrentPage(currentPage)
             })
     }
 
     render() {
+        console.log(this.props.totalUsersCount)
+        const usersCount = 5
+        const pagesCount = Array(Math.ceil((this.props.totalUsersCount / usersCount)))
+            .fill(0)
+            .map((e, i) => i + 1)
         return (
             <div className="Users">
+                {pagesCount.map((p, i) => <span key={i}
+                                                onClick={() => this.onPageChanged(p)}
+                                                className={this.props.currentPage === p ? 'currentPage' : ''}>{p}</span>)}
                 {this.props.users.map((u: any) => {
                     return (
-                        <div style={{padding: '10px 0'}}>
+                        <div key={u.id} style={{padding: '10px 0'}}>
                             <div>name: {u.name}</div>
                             <div>photo: <img src={u.photos.small} alt="нет фото"/></div>
                             <div>status: {u.status ? u.status : 'нет статуса'}</div>
@@ -35,7 +52,10 @@ class Users extends React.Component<any> {
 
 const mapStateToProps = (state: any) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        usersCount: state.usersPage.usersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 
@@ -49,6 +69,12 @@ const mapDispatchToProps = (dispatch: any) => {
         },
         unfollow: (userId: any) => {
             dispatch(unfollowAC(userId))
+        },
+        setTotalUsersCount: (totalUsersCount: any) => {
+            dispatch(setTotalUsersCountAC(totalUsersCount))
+        },
+        toggleCurrentPage: (currentPage: any) => {
+            dispatch(toggleCurrentPageAC(currentPage))
         }
     }
 }
--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -1,6 +1,8 @@
 const initState = {
     users: [],
-
+    totalUsersCount: 0,
+    usersCount: 5,
+    currentPage: 1
 }
 export const usersReducer = (state: any = initState, action: any) => {
     switch (action.type) {
@@ -13,8 +15,14 @@ export const usersReducer = (state: any = initState, action: any) => {
         case 'UNFOLLOW': {
             return {...state, users: state.users.map((u: any) => u.id === action.userId ? {...u, followed: false} : u)}
         }
+        case 'SET-TOTAL-USERS-COUNT': {
+            return {...state, totalUsersCount: action.totalUsersCount}
+        }
         default: {
             return state
+        }
+        case 'TOGGLE-CURRENT-PAGE': {
+            return {...state, currentPage: action.currentPage}
         }
     }
 }
@@ -23,3 +31,6 @@ export const usersReducer = (state: any = initState, action: any) => {
 export const setUsersAC = (users: any) => ({type: 'SET-USERS', users})
 export const followAC = (userId: any) => ({type: 'FOLLOW', userId})
 export const unfollowAC = (userId: any) => ({type: 'UNFOLLOW', userId})
+
+export const setTotalUsersCountAC = (totalUsersCount: any) => ({type: 'SET-TOTAL-USERS-COUNT', totalUsersCount})
+export const toggleCurrentPageAC = (currentPage: any) => ({type: 'TOGGLE-CURRENT-PAGE', currentPage})
