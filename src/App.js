6ab9d76ced2348891f7006347cfddcc7dc52a035
antonorlov <fogelo@yandex.ru>
2022-04-19 15:28:05 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input type="text"/>
+      <button>+</button>
+      <ul>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
